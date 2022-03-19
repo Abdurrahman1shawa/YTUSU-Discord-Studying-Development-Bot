6aa9e13472091461b824242a7823bbe454f1d639
Abdurrahman <free.abdo.sh@gmail.com>
2022-03-19 14:27:25 +0300

    Help functions added to the bot class

--- Bot.py
+++ Bot.py
@@ -42,3 +42,71 @@ class Bot(discord.Client):
 
         channel = self.get_channel(channel)
         await channel.send(f" <@{user_id}>Slow down! You're already have an ongoing timer :face_with_monocle:")
+
+    async def help(self, server, channel, admin = False):
+
+        if admin:
+            help = """
+                    **{prefix}<give/take> <@member> <x: number> <study/work>**
+                    Gives/Takes x study/work minutes from member.
+
+                    **{prefix}stopalltimers**
+                    Cancels and saves elapsed time of all ongoing timers.
+
+                    **{prefix}swroles <on/off>**
+                    Activates/Deactivates the studying and working roles.
+
+                    **{prefix}cleardatabase**
+                    Resets all saved server data on the <@> Beta database.
+
+                    **{prefix}my settings**
+                    Shows bot settings for the server.
+
+                    **{prefix}change prefix <prefix>**
+                    I mean it's obvious lol.
+
+                    **&autoreset <on/off>**
+                    Activates/Deactivates the auto reset mode.
+
+                    **&set logschannel**
+                    define the current channel as logs channel.
+
+                    **&autoreset datetime year-month-day hours:minutes:secnods**
+                    set the inital auto reset datetime.
+                    please consider that you are using the utc time.
+
+                    **&autoreset periode x**
+                    set the periode of the auto reset.
+
+                    """.format(prefix = server.prefix)
+            embed = discord.Embed(title = "Admin's Commands", description = help)
+
+
+        else:
+
+            help = """
+                    **{prefix}[study/work]**
+                    creates a 25 minutes study/work timer.
+
+                    **{prefix}[study/work] x**
+                    Creates an x minutes study/work timer. (10 < x < 120)
+
+                    **{prefix}[study/work] x break y**
+                    Creates an x minutes study/work timer, followed by a y minutes break timer.
+                    (10 < x < 120) (5 < y < 30)
+
+                    **{prefix}cancel**
+                    Cancels and saves (if exists) the remaining time of the ongoing timer.
+
+                    **{prefix}cancel clear**
+                    Cancels (if exists) the ongoing timer.
+
+                    **{prefix}rtime**
+                    Shows the remaining time for the end of the ongoing timer (if a timer exists).
+
+                    **{prefix}top**
+                    Shows the top 10 productive people of the server""".format(prefix = server.prefix)
+
+            embed = discord.Embed(title = "User's Commands", description = help)
+
+        await channel.send(embed = embed)
